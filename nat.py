93f11fcfd92dffac776825daa6a87913a803e4f7
Rihanashariff <52313932+Rihanashariff@users.noreply.github.com>
2019-07-03 15:30:08 +0530

    Update nat.py

--- nat.py
+++ nat.py
@@ -1,7 +1,8 @@
-r= int(input())
-sum = 0
-for r in range(0,r+1,1):
-  sum+=r
-print(sum)
+r=int(input("Enter a number: "))
+sum1 = 0
+while(r > 0):
+    sum1=sum1+r
+    r=r-1
+print("The sum of first n natural numbers is",sum1)
 
 
