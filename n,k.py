322086cb005f86e70bf586b021c780ed441111e1
Rihanashariff <52313932+Rihanashariff@users.noreply.github.com>
2019-07-03 15:39:15 +0530

    Update n,k.py

---
+++ n,k.py
@@ -0,0 +1,6 @@
+n,k=map(int,input().split())
+r=list(map(int,input().split()))
+m=0
+for i in range(k):
+    m=m+r[i]
+print(m)
--- nat.py
+++
@@ -1,8 +0,0 @@
-r=int(input("Enter a number: "))
-sum1 = 0
-while(r > 0):
-    sum1=sum1+r
-    r=r-1
-print("The sum of first n natural numbers is",sum1)
-
-
